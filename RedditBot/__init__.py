44b836957be6e3f35dab935ded37f8c55c5a6c86
Tom Powell <zifnab@zifnab06.net>
2013-11-19 22:37:31 -0700

    First attempt at dynamic plugin loading

--- RedditBot/__init__.py
+++ RedditBot/__init__.py
@@ -4,7 +4,7 @@ sys.path.insert(1, sys.path[0] + "/irctk")
 from RedditBot.redditbot import Bot
 from RedditBot.config import Config
 from copy import deepcopy
-
+from importlib import import_module
 # initialize and config the bot
 bot = Bot()
 bot.config.from_object(Config)
@@ -16,9 +16,9 @@ bot.load_config()
 
 
 # load our plugins
-from RedditBot.plugins import (reddit, twitter, botutils, youtube, badword, tell, minecraft, google,
-                               lastfm, config, kickrejoin, grab, wolframalpha, python,
-                               wikipedia, github, vimeo, steam, mumble, imdb, countdown, pun)
+pluginList = bot.config["PLUGINS"];
+for plugin in pluginList:
+    import_module(plugin, 'RedditBot.plugins')
 
 # Available plugins that aren't loaded by default
 # from RedditBot.plugins import eval
